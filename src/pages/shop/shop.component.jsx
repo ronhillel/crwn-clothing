0766f43e6b942a80e55cd84e954bfde4f65e92c4
Ron Hillel <drak372@gmail.com>
2021-04-27 18:36:24 +0300

    removing log to console of match object

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,15 +3,11 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => {
-    console.log(match);
-
-    return (
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
-        </div>
-    );
-}
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+    </div>
+);
 
 export default ShopPage;
